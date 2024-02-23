c697b44d25960a834ab165fdb061a71a9f40dbb7
Tommy Li <Tommy.chaoping.li@ibm.com>
2024-02-23 12:55:54 -0800

    remove L117

--- cli/chat/chat.py
+++ cli/chat/chat.py
@@ -114,7 +114,8 @@ class ConsoleChatBot():
         ('#85bb65 bold', f"[{self._total_tokens}]"), # dollar green for tokens
         ('#3f7cac bold', f"[{'M' if self.multiline else 'S'}]"), # info blue for multiple
         *([('bold', f"[{self.loaded['name']}]")] if "name" in self.loaded else []), # loaded context/session file
-        *([] if openai.proxy is None else [('#d08770 bold', "[proxied]")]), # indicate prox
+        # TODO: Fix openai package to fix the openai error.
+        # *([] if openai.proxy is None else [('#d08770 bold', "[proxied]")]), # indicate prox
     ])
 
     def _handle_quit(self, content):
