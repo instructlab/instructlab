eb35aec2da891359c21c7e25c832550567e7730c
Taj Salawu <71083414+tssala23@users.noreply.github.com>
2024-02-28 15:53:18 -0500

    Move check for prompt file Fix #105 (#115)
    * Fix issue #105 by moving check for prompt file
    
    Signed-off-by: tssala23 <tsalawu@redhat.com>
    
    * Fix variable names
    
    Signed-off-by: tssala23 <tsalawu@redhat.com>
    
    ---------
    
    Signed-off-by: tssala23 <tsalawu@redhat.com>

--- cli/generator/generate_data.py
+++ cli/generator/generate_data.py
@@ -21,7 +21,7 @@ from . import utils
 from ..config.config import Config
 
 
-DEFAULT_PROMPT = """\
+DEFAULT_PROMPT_TEMPLATE = """\
 You are asked to come up with a set of 20 diverse task instructions. These task instructions will be given to a GPT model and we will evaluate the GPT model for completing the instructions.
 
 Here are the requirements:
@@ -38,15 +38,18 @@ Here are the requirements:
 List of 20 tasks:
 """
 
-def encode_prompt(prompt_instructions, prompt_file):
-    """Encode multiple prompt instructions into a single string."""
+def check_prompt_file(prompt_file_path):
+    """Check for prompt file."""
     try:
-        prompt = open(prompt_file).read()
+        prompt_template = open(prompt_file_path).read()
     except:
-        print(f"cannot find {prompt_file}. using default prompt")
-        prompt = DEFAULT_PROMPT
-    prompt = prompt + "\n"
+        print(f"cannot find {prompt_file_path}. using default prompt")
+        prompt_template = DEFAULT_PROMPT_TEMPLATE
+    prompt_template = prompt_template + "\n"
+    return prompt_template
 
+def encode_prompt(prompt_instructions, prompt):
+    """Encode multiple prompt instructions into a single string."""
     for idx, task_dict in enumerate(prompt_instructions):
         (instruction, input, output) = task_dict["instruction"], task_dict["input"], task_dict["output"]
         instruction = re.sub(r"\s+", " ", instruction).strip().rstrip(":")
@@ -254,6 +257,7 @@ def generate_data(
     ]
     all_instruction_tokens = [scorer._tokenizer.tokenize(inst) for inst in all_instructions]
 
+    prompt_template = check_prompt_file(prompt_file_path)
     while len(machine_instruction_data) < num_instructions_to_generate:
         request_idx += 1
 
@@ -261,7 +265,7 @@ def generate_data(
         for _ in range(request_batch_size):
             # only sampling from the seed tasks
             prompt_instructions = random.sample(seed_instruction_data, num_prompt_instructions)
-            prompt = encode_prompt(prompt_instructions, prompt_file_path)
+            prompt = encode_prompt(prompt_instructions, prompt_template)
             batch_inputs.append(prompt)
         decoding_args = utils.OpenAIDecodingArguments(
             temperature=temperature,
