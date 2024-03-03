dec6a69e8b9dca2a6a4f6d41555c9a73bc0d7f6f
Jaideep Rao <jaideep.r97@gmail.com>
2024-03-03 01:42:38 -0500

    fill out train, test, convert methods for MLX (#214)
    * fill out train, test, convert methods; add way to test them
    
    * rename test_lab to temp_lab
    
    * delete test_lab.py
    
    * address review comments
    
    * reference lora requirements.txt

---
+++ cli/lab_test.py
@@ -0,0 +1,26 @@
+import temp_lab
+
+def test_convert(model_dir, adapter_file, quantized):
+    print("tesing convert")
+    temp_lab.convert(model_dir, adapter_file, quantized)
+
+def test_test(model_dir, adapter_file):
+    print("tesing test")
+    temp_lab.test(model_dir, adapter_file)
+
+def test_train(data_dir, model_dir, remote, quantize):
+    print("tesing train")
+    temp_lab.train(data_dir, model_dir, remote, quantize)
+
+if __name__ == "__main__":
+    train_model_dir = "train/lora-mlx/ibm-merlinite-7b"
+    model_dir = "train/lora-mlx/ibm-merlinite-7b-mlx-q"
+    adapter_file = "train/lora-mlx/ibm-merlinite-7b-mlx-q/adapters-010.npz"
+    quantized = True
+    remote = False
+    data_dir = "train/lora-mlx/data_puns_shiv"
+
+    test_train(data_dir, train_model_dir, remote, quantized)
+    test_test(model_dir, adapter_file)
+    test_convert(model_dir, adapter_file, quantized)
+    
