d3e03ebbcc37c935c95097ffb6bdd27643de9a31
markstur <mark.sturdevant@ibm.com>
2024-03-02 18:23:42 -0800

    Move default taxonomy branch default to config.
    It was hard-coded "main".
    But in releases, we've been setting the default to
    a version-compatible taxonomy branch in config.  Let's
    make main use config too.
    
    Signed-off-by: markstur <mark.sturdevant@ibm.com>

--- cli/config.py
+++ cli/config.py
@@ -14,6 +14,7 @@ DEFAULT_VI_MODE = False
 DEFAULT_VISIBLE_OVERFLOW = True
 DEFAULT_TAXONOMY_REPO = "git@github.com:open-labrador/taxonomy.git"
 DEFAULT_TAXONOMY_PATH = "taxonomy"
+DEFAULT_TAXONOMY_BRANCH = "main"
 DEFAULT_PROMPT_FILE = "prompt.txt"
 DEFAULT_SEED_FILE = "seed_tasks.json"
 
