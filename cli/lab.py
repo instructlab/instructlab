201b15f69879fceef074ff2acc326501d6b11fc6
Kai Xu <xuk@ibm.com>
2024-03-03 01:57:38 -0500

    fix: make happy path of mlx work (#215)
    * fix: make happy path of mlx work
    
    * docs: add mlx happy path

--- cli/lab.py
+++ cli/lab.py
@@ -1,4 +1,5 @@
 # Standard
+import os
 from os import listdir
 from os.path import basename, dirname, exists, splitext
 import logging
@@ -395,23 +396,38 @@ def download(ctx, repository, release, model_dir, pattern):
     "--model-dir", help="Base directory where model is stored.", default="ibm-merlinite-7b"
 )
 @click.option(
+    "--iters", help="Number of iterations to train LoRA", default=100
+)
+@click.option(
     "--remote",
     is_flag=True,
     help="Whether or not `model_dir` is remote from HuggingFace.",
 )
-def train(data_dir, model_dir, remote, quantize):
+@click.option(
+    "-q",
+    "--quantize",
+    is_flag=True,
+    help="Whether to do quantization while converting to MLX.",
+)
+def train(data_dir, model_dir, iters, remote, quantize):
     """
     Takes synthetic data generated locally with `lab generate` and the previous model and learns a new model using the MLX API.
     On success, writes newly learned model to {model_dir}/mlx_model, which is where `chatmlx` will look for a model.
     """
+    cli_dir = os.path.dirname(os.path.abspath(__file__))
+
+    script = os.path.join(cli_dir, "train/lora-mlx/make_data.py")
+    cmd = f"{script} --data-dir {data_dir}"
+    os.system('python {}'.format(cmd))
 
     is_macos = True # TODO detect OS
     if is_macos:
         # NOTE we can skip this if we have a way ship MLX
         # TODO convert the model from PyTorch to MLX
         # PyTorch safetensors to MLX safetensors
-        model_dir_mlx = f"{model_dir}-mlx"
-        model_dir_mlx_quantized = f"{model_dir}-mlx-q"
+        model_dir_local = model_dir.replace("/", "-")
+        model_dir_mlx = f"{model_dir_local}-mlx"
+        model_dir_mlx_quantized = f"{model_dir_local}-mlx-q"
 
         dest_model_dir = ""
         quantize_arg = ""
@@ -425,19 +441,19 @@ def train(data_dir, model_dir, remote, quantize):
         if not remote:
             local_arg = "--local"
 
-        script = os.path.join(os.getcwd(), "train/lora-mlx/convert.py")
+        script = os.path.join(cli_dir, "train/lora-mlx/convert.py")
         cmd = f"{script}  --hf-path {model_dir} --mlx-path {dest_model_dir} {quantize_arg} {local_arg}"
          # python convert.py --hf-path ibm-merlinite-7b --mlx-path ibm-merlinite-7b-mlx
         os.system('python {}'.format(cmd))
 
 
         adapter_file_path = f"{dest_model_dir}/adapters.npz"
-        script = os.path.join(os.getcwd(), "train/lora-mlx/lora.py")
+        script = os.path.join(cli_dir, "train/lora-mlx/lora.py")
         # TODO train the model with LoRA
         # python lora.py --model {{model_dir_mlx}} --train --data data_puns_shiv --adapter-file {{model_dir_mlx}}/adapters.npz --iters 300 --save-every 10 --steps-per-eval 10
         # exact command:
         # python lora.py --model ibm-merlinite-7b-mlx --train --data data_puns_shiv --adapter-file ibm-merlinite-7b-mlx/adapters.npz --iters 100 --save-every 10 --steps-per-eval 10
-        cmd = f"{script} --model {dest_model_dir} --train --data {data_dir} --adapter-file {adapter_file_path} --iters 300 --save-every 10 --steps-per-eval 10"
+        cmd = f"{script} --model {dest_model_dir} --train --data {data_dir} --adapter-file {adapter_file_path} --iters {iters} --save-every 10 --steps-per-eval 10"
         os.system('python {}'.format(cmd))
 
         # TODO copy some downloaded files from the PyTorch model folder
@@ -490,22 +506,32 @@ def test(data_dir, model_dir, adapter_file):
 @click.option(
     "--adapter-file", help="LoRA adapter to fuse."
 )
-def convert(model_dir, adapter_file, quantized=False):
-    
+@click.option(
+    "-d",
+    "--de-quantize",
+    help="Generate a de-quantized model.",
+    is_flag=True,
+)
+@click.option(
+    "-q",
+    "--quantize",
+    is_flag=True,
+    help="Whether to do quantization while converting to GGUF.",
+)
+def convert(model_dir, adapter_file, de_quantize, quantize):
     """
     TODO
     """
-    model_dir_mlx = f"{model_dir}-mlx"
-    model_dir_mlx_quantized = f"{model_dir}-mlx-q"
+    cli_dir = os.path.dirname(os.path.abspath(__file__))
 
     dequantize_arg = ""
     source_model_dir = model_dir
-    if quantized:
+    if de_quantize:
         dequantize_arg = " -d "
 
     model_dir_fused= f"{source_model_dir}-fused"
     
-    script = os.path.join(os.getcwd(), "train/lora-mlx/fuse.py")
+    script = os.path.join(cli_dir, "train/lora-mlx/fuse.py")
     # cmd = cwd + " --model " + source_model_dir + " --save-path " + model_dir_fused + " --adapter-file " + adapter_file_path + dequantize_arg
     cmd = f"{script} --model {source_model_dir} --save-path {model_dir_fused} --adapter-file {adapter_file} {dequantize_arg}"
     # this combines adapter with the original model to produce the updated model
@@ -515,7 +541,7 @@ def convert(model_dir, adapter_file, quantized=False):
 
     model_dir_fused_pt= f"{model_dir_fused}-pt"
 
-    script = os.path.join(os.getcwd(), "train/lora-mlx/convert.py ")
+    script = os.path.join(cli_dir, "train/lora-mlx/convert.py ")
     cmd = f"{script} --hf-path { model_dir_fused} --mlx-path {model_dir_fused_pt} --local --to-pt"
     # this converts MLX to PyTorch
     # python convert.py --hf-path {{model}} --local --mlx-path {{model}}-pt --to-pt
@@ -523,7 +549,7 @@ def convert(model_dir, adapter_file, quantized=False):
     os.system('{} {}'.format('python', cmd))
 
 
-    script = os.path.join(os.getcwd(), "llamacpp/llamacpp_convert_to_gguf.py")
+    script = os.path.join(cli_dir, "llamacpp/llamacpp_convert_to_gguf.py")
     cmd = f"{script} { model_dir_fused_pt} --pad-vocab"
     # use llama.cpp to convert back to GGUF
     # python $HOME/src/open-labrador/llama.cpp/convert.py {{model_dir}} --pad-vocab
@@ -533,9 +559,9 @@ def convert(model_dir, adapter_file, quantized=False):
     # quantize 4-bi GGUF (optional)
     # $HOME/src/open-labrador/llama.cpp/quantize {{model_dir}}/ggml-model-f16.gguf {{model_dir}}/ggml-model-Q4_K_M.gguf Q4_K_M
     # TO DO: fix this to execute function instead
-    if quantized:
+    if quantize:
         gguf_model_dir = f"{model_dir_fused_pt}/ggml-model-f16.gguf" 
         gguf_model_q_dir = f"{model_dir_fused_pt}/ggml-model-Q4_K_M.gguf"
-        script = os.path.join(os.getcwd(), "llamacpp/quantize")
+        script = os.path.join(cli_dir, "llamacpp/quantize")
         cmd = f"{script} {gguf_model_dir} {gguf_model_q_dir} Q4_K_M"
         os.system('{}'.format(cmd))
