dbf40842930a45327fc1f50ea176eb0565d7ed38
Anik Bhattacharjee <anbhatta@redhat.com>
2024-03-04 15:30:36 -0500

    resolve conflicts

--- tests/test_lab_init.py
+++ tests/test_lab_init.py
@@ -3,9 +3,8 @@ import cli.lab as lab
 
 def test_init():
     runner = CliRunner()
-    lab_test = lab
     with runner.isolated_filesystem():
-        result = CliRunner().invoke(lab_test.init, ["--interactive", "--model-path", "models", "--taxonomy-path", "taxonomy"])
+        result = CliRunner().invoke(lab.init, ["--interactive", "--taxonomy-path", "taxonomy"])
         assert result.exit_code == 0   
 
         result = CliRunner().invoke(lab.init)
