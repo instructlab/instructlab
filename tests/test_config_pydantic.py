1e79576dd5591491cfa892c449afc995b39e074f
Anik Bhattacharjee <anbhatta@redhat.com>
2024-03-04 15:30:36 -0500

    resolve conflicts

--- tests/test_config_pydantic.py
+++ tests/test_config_pydantic.py
@@ -1,28 +1,21 @@
-# local
-# Third Party
-# 3rd party
+# Standard
 import pydantic
-import pydantic_yaml  # YAML parsing utility
+import pydantic_yaml
+
+# 3rd party
 from click.testing import CliRunner
 
-# First Party
+# Local
 from tests.schema import Config
 import cli.lab as lab
 
 
 def test_config_pydantic():
-    """
-    Assumes for the moment:
-        (1) config.yaml is at /cli/config.yaml of project
-        (2) `pytest` is run from root of project.
-    """
-
-    # check if config.yaml is here
     runner = CliRunner()
     with runner.isolated_filesystem():
-        result = CliRunner().invoke(lab.init, ["--interactive", "--model-path", "models", "--taxonomy-path", "taxonomy"])
+        result = CliRunner().invoke(lab.init, ["--interactive", "--taxonomy-path", "taxonomy"])
         try:
-            parsed = pydantic_yaml.parse_yaml_file_as(model_type=Config, file="config.yaml")
+            pydantic_yaml.parse_yaml_file_as(model_type=Config, file="config.yaml")
             # If config.yaml parses to Config pydantic model, success.
             assert True
         except TypeError as e:
@@ -30,4 +23,6 @@ def test_config_pydantic():
             assert False
         except pydantic.ValidationError as e:
             print(e)
+            assert False
+        except:
             assert False
