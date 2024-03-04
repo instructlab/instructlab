9d35533da3d0a0d4a0f8473684b9b015b13db0af
James Kunstle <jkunstle@redhat.com>
2024-03-04 11:49:34 -0500

    adds basic testing files
    Signed-off-by: James Kunstle <jkunstle@redhat.com>

---
+++ tests/schema.py
@@ -0,0 +1,54 @@
+# Standard
+import enum
+
+# Third Party
+from pydantic import BaseModel
+
+
+class LogLevel(str, enum.Enum):
+    info = "INFO"
+    debug = "DEBUG"
+    warning = "WARNING"
+    error = "ERROR"
+    critical = "CRITICAL"
+
+
+class Chat(BaseModel):
+    api_base: str
+    api_key: str
+    context: str
+    session: str | None
+    vi_mode: bool
+    visible_overflow: bool
+
+
+class General(BaseModel):
+    log_level: LogLevel
+
+
+class Generate(BaseModel):
+    model: str
+    num_cpus: int
+    num_instructions: int
+    prompt_file: str
+    seed_file: str
+    taxonomy_path: str
+
+
+class List(BaseModel):
+    taxonomy_path: str
+
+
+class Serve(BaseModel):
+    gpu_layers: int
+    model_path: str
+
+
+class Config(BaseModel):
+    """Root of Pydantic schema"""
+
+    chat: Chat
+    general: General
+    generate: Generate
+    list: List
+    serve: Serve
