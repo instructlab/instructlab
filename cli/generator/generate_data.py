cebcac052ac4f1313f3f815742f3c8f35ffa7234
Ali Maredia <amaredia@redhat.com>
2024-03-07 03:28:39 -0500

    remove return value from read_taxonomy
    Error caught by linter
    
    Signed-off-by: Ali Maredia <amaredia@redhat.com>

--- cli/generator/generate_data.py
+++ cli/generator/generate_data.py
@@ -185,7 +185,7 @@ def generate_data(
     # throw an error if both not found
     # pylint: disable=broad-exception-caught,raise-missing-from
     if taxonomy and os.path.exists(taxonomy):
-        seed_instruction_data, tax_dir = read_taxonomy(logger, taxonomy)
+        seed_instruction_data = read_taxonomy(logger, taxonomy)
     elif seed_tasks_path and os.path.exists(seed_tasks_path):
         with open(seed_tasks_path, "r", encoding="utf-8") as seed_tasks_file:
             seed_tasks = [json.loads(l) for l in seed_tasks_file]
@@ -443,11 +443,8 @@ def read_taxonomy_file(logger, file_path):
 
 def read_taxonomy(logger, taxonomy):
     seed_instruction_data = []
-    output_dir = None
     is_file = os.path.isfile(taxonomy)
     if is_file:
-        # Default output_dir to taxonomy file's dir
-        output_dir = os.path.dirname(os.path.abspath(taxonomy))
         seed_instruction_data, warnings, errors = read_taxonomy_file(logger, taxonomy)
         if warnings:
             logger.warn(
@@ -456,8 +453,6 @@ def read_taxonomy(logger, taxonomy):
         if errors:
             raise SystemExit(yaml.YAMLError("Taxonomy file with errors! Exiting."))
     else:  # taxonomy is_dir
-        # Default output_dir to taxonomy dir, using the dir not the parent
-        output_dir = os.path.abspath(taxonomy)
         # Gather the new or changed YAMLs using git diff
         try:
             updated_taxonomy_files = get_taxonomy_diff(taxonomy)
@@ -480,4 +475,4 @@ def read_taxonomy(logger, taxonomy):
             raise SystemExit(
                 yaml.YAMLError(f"{total_errors} taxonomy files with errors! Exiting.")
             )
-    return seed_instruction_data, output_dir
+    return seed_instruction_data
