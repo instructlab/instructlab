8d3714b88613c6749197aa15f85e1f0fe0a9b2d6
Ihar Hrachyshka <ihrachys@redhat.com>
2024-03-11 11:17:46 -0400

    utils: close files properly (#511)
    Before the patch, if we raise an exception in jdump, the file will never
    close. jload may also leave a file handler open if .load() fails.
    
    Signed-off-by: Ihar Hrachyshka <ihrachys@redhat.com>

--- cli/generator/utils.py
+++ cli/generator/utils.py
@@ -163,19 +163,16 @@ def jdump(obj, f, mode="w", indent=4, default=str):
         indent: Indent for storing json dictionaries.
         default: A function to handle non-serializable entries; defaults to `str`.
     """
-    f = _make_w_io_base(f, mode)
-    if isinstance(obj, (dict, list)):
-        json.dump(obj, f, indent=indent, default=default)
-    elif isinstance(obj, str):
-        f.write(obj)
-    else:
-        raise ValueError(f"Unexpected type: {type(obj)}")
-    f.close()
+    with _make_w_io_base(f, mode) as f_:
+        if isinstance(obj, (dict, list)):
+            json.dump(obj, f_, indent=indent, default=default)
+        elif isinstance(obj, str):
+            f_.write(obj)
+        else:
+            raise ValueError(f"Unexpected type: {type(obj)}")
 
 
 def jload(f, mode="r"):
     """Load a .json file into a dictionary."""
-    f = _make_r_io_base(f, mode)
-    jdict = json.load(f)
-    f.close()
-    return jdict
+    with _make_r_io_base(f, mode) as f_:
+        return json.load(f_)
