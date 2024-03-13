3bfa9b4bb27bff6b428def69695218457d65576c
Derek Higgins <derekh@redhat.com>
2024-03-13 00:08:43 +0000

    Fix typos in Prompt Templates
    Signed-off-by: Derek Higgins <derekh@redhat.com>

--- cli/generator/generate_data.py
+++ cli/generator/generate_data.py
@@ -39,7 +39,7 @@ Here are the requirements:
 5. The instructions should be in English.
 6. The instructions should be 1 to 2 sentences long. Either an imperative sentence or a question is permitted.
 7. You should generate an appropriate input to the instruction. The input field should contain a specific example provided for the instruction. It should involve realistic data and should not contain simple placeholders. The input should provide substantial content to make the instruction challenging but should ideally not exceed 100 words.
-8. Not all instructions require input. For example, when a instruction asks about some general information, "what is the highest peak in the world", it is not necessary to provide a specific context. In this case, we simply put "<noinput>" in the input field.
+8. Not all instructions require input. For example, when an instruction asks about some general information, "what is the highest peak in the world", it is not necessary to provide a specific context. In this case, we simply put "<noinput>" in the input field.
 9. The output should be an appropriate response to the instruction and the input. Make sure the output is less than 100 words.
 
 List of 5 tasks:
@@ -50,12 +50,12 @@ You are asked to come up with a set of 5 diverse task instructions under {taxono
 
 Here are the requirements:
 1. Try not to repeat the verb for each instruction to maximize diversity.
-2. The language used for the instruction also should be diverse. For example, you should combine questions with imperative instrucitons.
+2. The language used for the instruction also should be diverse. For example, you should combine questions with imperative instructions.
 3. The type of instructions should be similar to provided examples. The generated instruction and the output should be grounded in the provided document.
 4. A GPT language model should be able to complete the instruction. For example, do not ask the assistant to create any visual or audio output. For another example, do not ask the assistant to wake you up at 5pm or set a reminder because it cannot perform any action.
 5. The instructions should be in English.
 6. The instructions should be 1 to 2 sentences long. Either an imperative sentence or a question is permitted.
-7. The output should be an appropriate response to the input and the instruction. Long outputs are preferrable. 
+7. The output should be an appropriate response to the input and the instruction. Long outputs are preferable.
 
 Based on below document provide a list of 5 tasks:
 
