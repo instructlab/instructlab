dfaf8b21434955e977740069367d69081d562a62
Maciej Szulik <maszulik@redhat.com>
2024-03-14 14:14:51 +0100

    Improve lab init and its tests
    This commit introduces the following chagnes:
    1. Makes sure not to clone taxonomy in non-interactive mode when
       it already exists
    2. Adds a message when git clone failed informing user to manually clone
       the repo
    3. Adds multiple tests for both interactive and non-interactive lab init
    
    Signed-off-by: Maciej Szulik <maszulik@redhat.com>

--- tests/test_lab_init.py
+++ tests/test_lab_init.py
@@ -11,6 +11,7 @@ import pydantic_yaml
 
 # First Party
 from cli import lab
+from cli.config import read_config
 from tests.schema import Config
 
 
@@ -18,19 +19,27 @@ class TestLabInit(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    @patch("git.Repo.clone_from", MagicMock())
-    def test_init(self):
+    # When using `from X import Y` you need to understand that Y becomes part
+    # of your module, so you should use `my_modyle.Y`` to patch.
+    # When using `import X`, you should use `X.Y` to patch.
+    # https://docs.python.org/3/library/unittest.mock.html#where-to-patch?
+    @patch("cli.lab.Repo.clone_from")
+    def test_init_noninteractive(self, mock_clone_from):
         runner = CliRunner()
         with runner.isolated_filesystem():
-            result = runner.invoke(lab.init, ["--non-interactive"])
+            result = runner.invoke(lab.init, args=["--non-interactive"])
             self.assertEqual(result.exit_code, 0)
-            self.assertTrue("config.yaml" in os.listdir())
+            self.assertIn("config.yaml", os.listdir())
+            mock_clone_from.assert_called_once()
 
-    @patch("git.Repo.clone_from", MagicMock())
-    def test_config_pydantic(self):
+    @patch("cli.lab.Repo.clone_from")
+    def test_init_config(self, mock_clone_from):
         runner = CliRunner()
         with runner.isolated_filesystem():
-            runner.invoke(lab.init, ["--non-interactive"])
+            result = runner.invoke(lab.init, args=["--non-interactive"])
+            self.assertEqual(result.exit_code, 0)
+            self.assertIn("config.yaml", os.listdir())
+            mock_clone_from.assert_called_once()
             try:
                 pydantic_yaml.parse_yaml_file_as(model_type=Config, file="config.yaml")
                 self.assertTrue
@@ -41,14 +50,67 @@ class TestLabInit(unittest.TestCase):
                 print(e)
                 assert self.assertFalse
 
-    @patch(
-        "git.Repo.clone_from", MagicMock(side_effect=GitError("Authentication failed"))
-    )
-    def test_init_git_error(self):
+    def test_init_interactive(self):
         runner = CliRunner()
         with runner.isolated_filesystem():
-            result = runner.invoke(lab.init, ["--non-interactive"])
-            self.assertEqual(result.exit_code, 1)
-            self.assertTrue(
-                "Failed to clone taxonomy repo: Authentication failed" in result.output
+            result = runner.invoke(lab.init, input="\nn")
+            self.assertEqual(result.exit_code, 0)
+            self.assertIn("config.yaml", os.listdir())
+
+    @patch(
+        "cli.lab.Repo.clone_from",
+        MagicMock(side_effect=GitError("Authentication failed")),
+    )
+    def test_init_interactive_git_error(self):
+        runner = CliRunner()
+        with runner.isolated_filesystem():
+            result = runner.invoke(lab.init, input="\ny")
+            self.assertEqual(
+                result.exit_code, 1, "command finished with an unexpected exit code"
             )
+            self.assertIn(
+                "Failed to clone taxonomy repo: Authentication failed", result.output
+            )
+            self.assertIn("manually run", result.output)
+
+    @patch("cli.lab.Repo.clone_from")
+    def test_init_interactive_clone(self, mock_clone_from):
+        runner = CliRunner()
+        with runner.isolated_filesystem():
+            result = runner.invoke(lab.init, input="\ny")
+            self.assertEqual(result.exit_code, 0)
+            self.assertIn("config.yaml", os.listdir())
+            mock_clone_from.assert_called_once()
+
+    def test_init_interactive_with_preexisting_nonempty_taxonomy(self):
+        runner = CliRunner()
+        with runner.isolated_filesystem():
+            os.makedirs("taxonomy/contents")
+            result = runner.invoke(lab.init, input="\n")
+            self.assertEqual(result.exit_code, 0)
+            self.assertIn("config.yaml", os.listdir())
+            self.assertIn("taxonomy", os.listdir())
+
+    def test_init_interactive_with_preexisting_config(self):
+        runner = CliRunner()
+        with runner.isolated_filesystem():
+            # first run to prime the config.yaml in current directory
+            result = runner.invoke(lab.init, input="non-default-taxonomy\nn")
+            self.assertEqual(result.exit_code, 0)
+            self.assertIn("config.yaml", os.listdir())
+            config = read_config("config.yaml")
+            self.assertEqual(config.generate.taxonomy_path, "non-default-taxonomy")
+
+            # second invocation should ask if we want to overwrite - yes, and change taxonomy path
+            result = runner.invoke(lab.init, input="y\ndifferent-taxonomy\nn")
+            self.assertEqual(result.exit_code, 0)
+            self.assertIn("config.yaml", os.listdir())
+            config = read_config("config.yaml")
+            self.assertEqual(config.generate.taxonomy_path, "different-taxonomy")
+
+            # third invocation should again ask, but this time don't overwrite
+            result = runner.invoke(lab.init, input="n")
+            self.assertEqual(result.exit_code, 0)
+            self.assertIn("config.yaml", os.listdir())
+            config = read_config("config.yaml")
+            self.assertEqual(config.generate.taxonomy_path, "different-taxonomy")
