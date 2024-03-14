e37df463fa624245926fc32098840a2f2b937c58
Maciej Szulik <maszulik@redhat.com>
2024-03-14 18:27:59 +0100

    Use proper patching logic in download tests (#618)
    Signed-off-by: Maciej Szulik <maszulik@redhat.com>

--- tests/test_lab_download.py
+++ tests/test_lab_download.py
@@ -9,29 +9,31 @@ from huggingface_hub.utils import HfHubHTTPError
 # First Party
 from cli import lab
 
-server_error = HfHubHTTPError("Could not reach hugging face server")
-
-
-def mock_download_raise_exception(repo_id, revision, filename, local_dir):
-    raise server_error
-
 
 class TestLabDownload(unittest.TestCase):
-    @patch("huggingface_hub.hf_hub_download")
-    def test_download(self, _):
+    # When using `from X import Y` you need to understand that Y becomes part
+    # of your module, so you should use `my_module.Y`` to patch.
+    # When using `import X`, you should use `X.Y` to patch.
+    # https://docs.python.org/3/library/unittest.mock.html#where-to-patch?
+    @patch("cli.lab.hf_hub_download")
+    def test_download(self, mock_hf_hub_download):
         runner = CliRunner()
         with runner.isolated_filesystem():
-            lab.hf_hub_download = MagicMock()
             result = runner.invoke(lab.download)
-            self.assertEqual(result.exit_code, 0)
-
-    @patch("huggingface_hub.hf_hub_download")
-    def test_download_error(self, _):
-        runner = CliRunner()
-        with runner.isolated_filesystem():
-            lab.hf_hub_download = mock_download_raise_exception
-            result = runner.invoke(lab.download)
-            self.assertEqual(result.exit_code, 1)
-            self.assertTrue(
-                f"Downloading model failed with the following Hugging Face Hub error: {server_error}"
+            self.assertEqual(
+                result.exit_code, 0, "command finished with an unexpected exit code"
             )
+            mock_hf_hub_download.assert_called_once()
+
+    @patch(
+        "cli.lab.hf_hub_download",
+        MagicMock(side_effect=HfHubHTTPError("Could not reach hugging face server")),
+    )
+    def test_download_error(self):
+        runner = CliRunner()
+        with runner.isolated_filesystem():
+            result = runner.invoke(lab.download)
+            self.assertEqual(
+                result.exit_code, 1, "command finished with an unexpected exit code"
+            )
+            self.assertIn("Could not reach hugging face server", result.output)
