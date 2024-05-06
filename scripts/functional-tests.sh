64115c4f2a8a02f4e8a33dfce4dbfd37fa7f5098
Sébastien Han <seb@redhat.com>
2024-05-06 11:36:35 +0200

    Fix up model name checking in the functional tests
    This test was failing at the moment because it would spawn `ilab
    serve` in the background and then immediately rename the model file. I
    saw it failing because `ilab serve` was looking for the original
    filename, but it tried to open it too late.
    
    Switch the operations and test cases around a bit to be a bit more
    deterministic and to match the current behavior of the code.
    
    Co-authored-by: Russell Bryant <rbryant@redhat.com>
    Signed-off-by: Sébastien Han <seb@redhat.com>

--- scripts/functional-tests.sh
+++ scripts/functional-tests.sh
@@ -40,7 +40,8 @@ cleanup() {
     sed -i.bak 's/9999/8000/g' config.yaml
     rm -f config.yaml.bak
     # revert model name change from test_model_print()
-    sed -i "s/baz/merlinite-7b-lab-Q4_K_M/g" config.yaml
+    sed -i.bak "s/baz/merlinite-7b-lab-Q4_K_M/g" config.yaml
+    mv models/foo.gguf models/merlinite-7b-lab-Q4_K_M.gguf
     set -e
 }
 
@@ -315,39 +316,58 @@ wait_for_server(){
 }
 
 test_model_print(){
-    ilab serve &
-    PID_SERVE=$!
     mv models/merlinite-7b-lab-Q4_K_M.gguf models/foo.gguf
+    ilab serve --model-path models/foo.gguf &
+    PID_SERVE=$!
+
+    wait_for_server
+
+    # validate that we print the model from the server since it is different from the config
+    expect -c '
+        spawn ilab chat
+        expect {
+            -re "Welcome to InstructLab Chat w/ \\\u001b\\\[1mMODELS/FOO\\.GGUF" { exit 0 }
+            eof { catch wait result; exit [lindex $result 3] }
+            timeout { exit 1 }
+        }
+    '
 
     # validate that we print the model from the CLI
     expect -c '
         set timeout 30
         spawn ilab chat -m bar
         expect {
-            "Welcome to InstructLab Chat w/ BAR" { exit 0 }
+            -re "Welcome to InstructLab Chat w/ \\\u001b\\\[1mBAR\\\u001b\\\[0m" { exit 0 }
+            eof { catch wait result; exit [lindex $result 3] }
             timeout { exit 1 }
         }
     '
 
     # validate that we print the model from the config
     expect -c '
-        sed -i "s/merlinite-7b-lab-Q4_K_M/baz/g" config.yaml
+        exec sed -i.bak "s/merlinite-7b-lab-Q4_K_M/baz/g" config.yaml
         spawn ilab chat
         expect {
-            "Welcome to InstructLab Chat w/ BAZ" { exit 0 }
+            -re "Welcome to InstructLab Chat w/ \\\u001b\\\[1mBAZ\\\u001b\\\[0m" {
+                exec sed -i.bak "s/baz/merlinite-7b-lab-Q4_K_M/g" config.yaml
+                exit 0
+            }
+            eof { catch wait result; exit [lindex $result 3] }
             timeout { exit 1 }
         }
-        sed -i "s/baz/merlinite-7b-lab-Q4_K_M/g" config.yaml
     '
 
-    # validate that we print the model from the server since it is different from the config
+    # If we don't specify a model, validate that we print the model reported
+    # by the server since it is different from the config.
     expect -c '
         spawn ilab chat
         expect {
-            "Welcome to InstructLab Chat w/ MODELS/FOO.GGUF" { exit 0 }
+            -re "Welcome to InstructLab Chat w/ \\\u001b\\\[1mMODELS/FOO\\.GGUF" { exit 0 }
+            eof { catch wait result; exit [lindex $result 3] }
             timeout { exit 1 }
         }
     '
+
 }
 
 ########
