48c0361393c9851c51fa5061e916fdd64207b9a1
Rafael Vasquez <rafvasq21@gmail.com>
2024-05-24 13:41:06 -0400

    Revert changes to init tests
    Signed-off-by: Rafael Vasquez <rafvasq21@gmail.com>

--- tests/test_lab_init.py
+++ tests/test_lab_init.py
@@ -31,7 +31,7 @@ class TestLabInit(unittest.TestCase):
     def test_init_interactive(self):
         runner = CliRunner()
         with runner.isolated_filesystem():
-            result = runner.invoke(lab.init, input="\n\nn")
+            result = runner.invoke(lab.init, input="\nn")
             self.assertEqual(result.exit_code, 0)
             self.assertIn("config.yaml", os.listdir())
 
@@ -42,7 +42,7 @@ class TestLabInit(unittest.TestCase):
     def test_init_interactive_git_error(self):
         runner = CliRunner()
         with runner.isolated_filesystem():
-            result = runner.invoke(lab.init, input="\n\ny")
+            result = runner.invoke(lab.init, input="\ny")
             self.assertEqual(
                 result.exit_code, 1, "command finished with an unexpected exit code"
             )
@@ -55,7 +55,7 @@ class TestLabInit(unittest.TestCase):
     def test_init_interactive_clone(self, mock_clone_from):
         runner = CliRunner()
         with runner.isolated_filesystem():
-            result = runner.invoke(lab.init, input="\n\ny")
+            result = runner.invoke(lab.init, input="\ny")
             self.assertEqual(result.exit_code, 0)
             self.assertIn("config.yaml", os.listdir())
             mock_clone_from.assert_called_once()
@@ -64,7 +64,7 @@ class TestLabInit(unittest.TestCase):
         runner = CliRunner()
         with runner.isolated_filesystem():
             os.makedirs("taxonomy/contents")
-            result = runner.invoke(lab.init, input="\n\n")
+            result = runner.invoke(lab.init, input="\n")
             self.assertEqual(result.exit_code, 0)
             self.assertIn("config.yaml", os.listdir())
             self.assertIn("taxonomy", os.listdir())
@@ -73,21 +73,21 @@ class TestLabInit(unittest.TestCase):
         runner = CliRunner()
         with runner.isolated_filesystem():
             # first run to prime the config.yaml in current directory
-            result = runner.invoke(lab.init, input="\nnon-default-taxonomy\nn")
+            result = runner.invoke(lab.init, input="non-default-taxonomy\nn")
             self.assertEqual(result.exit_code, 0)
             self.assertIn("config.yaml", os.listdir())
             config = read_config("config.yaml")
             self.assertEqual(config.generate.taxonomy_path, "non-default-taxonomy")
 
             # second invocation should ask if we want to overwrite - yes, and change taxonomy path
-            result = runner.invoke(lab.init, input="y\n\ndifferent-taxonomy\nn")
+            result = runner.invoke(lab.init, input="y\ndifferent-taxonomy\nn")
             self.assertEqual(result.exit_code, 0)
             self.assertIn("config.yaml", os.listdir())
             config = read_config("config.yaml")
             self.assertEqual(config.generate.taxonomy_path, "different-taxonomy")
 
             # third invocation should again ask, but this time don't overwrite
-            result = runner.invoke(lab.init, input="n\n")
+            result = runner.invoke(lab.init, input="n")
             self.assertEqual(result.exit_code, 0)
             self.assertIn("config.yaml", os.listdir())
             config = read_config("config.yaml")
