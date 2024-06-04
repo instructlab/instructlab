72d3cf24afe9af70a4189c20ab6283bfe176b875
Ihar Hrachyshka <ihrachys@redhat.com>
2024-06-04 23:15:24 +0000

    Fix ssl config when only cert file is passed
    httpx.Client expects cert argument to be one of: a 2+ tuple, or a
    string, or None.
    
    Before the change, we were passing a 1-tuple when only the cert file was
    configured through cli, which is violating the typing requirements of
    the library; and it also made the library not initialize SSL context at
    all. See:
    
    https://github.com/encode/httpx/blob/37593c1952f4972040f6163da67e3777fd3d2e94/httpx/_config.py#L183
    Signed-off-by: Ihar Hrachyshka <ihrachys@redhat.com>

--- src/instructlab/utils.py
+++ src/instructlab/utils.py
@@ -599,5 +599,5 @@ def read_taxonomy(logger, taxonomy, taxonomy_base, yaml_rules):
 
 
 def get_ssl_cert_config(tls_client_cert, tls_client_key, tls_client_passwd):
-    orig_cert = (tls_client_cert, tls_client_key, tls_client_passwd)
-    return tuple(item for item in orig_cert if item)
+    if tls_client_cert:
+        return tls_client_cert, tls_client_key, tls_client_passwd
