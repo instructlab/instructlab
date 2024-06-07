instructlab/instructlab

commit bf332e84a3ddeaa4150bbeb6fb05648d6d0e8537

option --model-family forces model_family
Extract model_family from model_path automatically by default.
--model-family forces model_family.

fixes #1060

Signed-off-by: Costa Shulyupin <costa.shul@redhat.com>

src/instructlab/config.py
@@ -1,6 +1,8 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # Standard
+from os import path
+from re import match
 from typing import Optional
 
 # Third Party
@@ -190,3 +192,7 @@ def write_config(cfg, config_file=DEFAULT_CONFIG):
 def get_api_base(host_port):
     """Returns server API URL, based on the provided host_port"""
     return f"http://{host_port}/v1"
+
+
+def get_model_family(forced, model_path):
+    return forced if forced else match(r"^\w*", path.basename(model_path)).group(0)

src/instructlab/generator/generate_data.py
@@ -20,7 +20,7 @@ import click
 import tqdm
 
 # Local
-from ..config import DEFAULT_MULTIPROCESSING_START_METHOD
+from ..config import DEFAULT_MULTIPROCESSING_START_METHOD, get_model_family
 from ..utils import chunk_document, read_taxonomy
 from . import utils
 from .utils import GenerateException
@@ -449,7 +449,9 @@ def generate_data(
         scorer._tokenizer.tokenize(inst) for inst in all_instructions
     ]
 
-    prompt_template = check_prompt_file(prompt_file_path, model_family)
+    prompt_template = check_prompt_file(
+        prompt_file_path, get_model_family(model_family, model_name)
+    )
     if console_output:
         print(
             "Synthesizing new instructions. If you aren't satisfied with the generated instructions, interrupt training (Ctrl-C) and try adjusting your YAML files. Adding more examples may help."

src/instructlab/lab.py
@@ -307,8 +307,7 @@ utils.make_lab_diff_aliases(cli, diff)
 @click.option(
     "--model-family",
     type=str,
-    default="merlinite",
-    help="Model family is used to specify which chat template to serve with",
+    help="Force model family to specify which chat template to serve with",
 )
 @click.pass_context
 def serve(ctx, model_path, gpu_layers, num_threads, max_ctx_size, model_family):
@@ -449,8 +448,7 @@ def serve(ctx, model_path, gpu_layers, num_threads, max_ctx_size, model_family):
 )
 @click.option(
     "--model-family",
-    default="merlinite",
-    help="model family to use when picking a generation template",
+    help="Force model family to use when picking a generation template",
 )
 @click.pass_context
 def generate(
@@ -638,9 +636,7 @@ def generate(
 )
 @click.option(
     "--model-family",
-    default="merlinite",
-    show_default=True,
-    help="model family to use when picking a chat template",
+    help="Force model family to use when picking a chat template",
 )
 @click.pass_context
 def chat(

src/instructlab/server.py
@@ -20,7 +20,7 @@ import uvicorn
 
 # Local
 from .client import ClientException, list_models
-from .config import get_api_base
+from .config import get_api_base, get_model_family
 
 templates = [
     {
@@ -181,7 +181,7 @@ def server(
     eos_token = "<|endoftext|>"
     bos_token = ""
     for template_dict in templates:
-        if template_dict["model"] == model_family:
+        if template_dict["model"] == get_model_family(model_family, model_path):
             template = template_dict["template"]
             if template_dict["model"] == "mixtral":
                 eos_token = "</s>"

commit 61a2b6169ebf2641af2f0da7b03db3b2e684a322

Use correct model family for granite
This change fixes a regression introduced in commit
bf332e84a3ddeaa4150bbeb6fb05648d6d0e8537 from PR #1174.

Prior to this commit, ilab would correctly use the `merlinte` model
family when using a granite model as that was the default. The change
in #1174 added some additional logic trying to guess the model family
based on filename to avoid having to specify `--model-family` in some
cases.

This change made the code automatically set the model family to
`granite`, which the code did not understand, which resulted in broken
model behavior.

The code has been updated to do the following:

- Understand that `granite` maps to `merlinite` for the model family

- When guessing a model family based on filename, don't assume the
  result is valid. Validate it against the set of known model families
  to `ilab` first.

- Fall back to the previous default of `merlinite` if the guess
  doesn't result in something `ilab` understands.

Fixes #1281

Signed-off-by: Russell Bryant <rbryant@redhat.com>

src/instructlab/config.py
@@ -38,6 +38,17 @@ DEFAULT_CONNECTION_TIMEOUT = httpx.Timeout(timeout=30.0)
 # use spawn start method, fork is not thread-safe
 DEFAULT_MULTIPROCESSING_START_METHOD = "spawn"
 
+# When otherwise unknown, ilab uses this as the default family
+DEFAULT_MODEL_FAMILY = "merlinite"
+
+# Model families understood by ilab
+MODEL_FAMILIES = set(("merlinite", "mixtral"))
+
+# Map model names to their family
+MODEL_FAMILY_MAPPINGS = {
+    "granite": "merlinite",
+}
+
 
 class ConfigException(Exception):
     """An exception that a configuration file has an error."""
@@ -197,4 +208,11 @@ def get_api_base(host_port):
 
 
 def get_model_family(forced, model_path):
-    return forced if forced else match(r"^\w*", path.basename(model_path)).group(0)
+    if forced and forced.lower() not in MODEL_FAMILIES:
+        raise ConfigException("Unknown model family: %s" % forced)
+
+    # Try to guess the model family based on the model's filename
+    guess = match(r"^\w*", path.basename(model_path)).group(0).lower()
+    guess = MODEL_FAMILY_MAPPINGS.get(guess, guess)
+
+    return guess if guess in MODEL_FAMILIES else DEFAULT_MODEL_FAMILY