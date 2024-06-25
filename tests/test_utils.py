bfe5506fcc357deffc2986de490e2426b54ac111
Russell Bryant <rbryant@redhat.com>
2024-06-25 10:30:55 -0400

    Drop some more sdg-only code
    This code was only used by the sdg library. It has been moved there
    and is no longer needed here as of instructlab-sdg 0.0.4.
    
    Signed-off-by: Russell Bryant <rbryant@redhat.com>

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,56 +5,14 @@ from unittest.mock import Mock, patch
 
 # Third Party
 import git
-import pytest
 import yaml
 
 # First Party
 from instructlab import utils
 
-# Local
-from .testdata import testdata
-
 
 class TestUtils:
     """Test collection in instructlab.utils."""
-
-    def test_chunk_docs_wc_exeeds_ctx_window(self):
-        with pytest.raises(ValueError) as exc:
-            utils.chunk_document(
-                documents=testdata.documents,
-                chunk_word_count=1000,
-                server_ctx_size=1034,
-            )
-        assert (
-            "Given word count (1000) per doc will exceed the server context window size (1034)"
-            in str(exc.value)
-        )
-
-    def test_chunk_docs_chunk_overlap_error(self):
-        with pytest.raises(ValueError) as exc:
-            utils.chunk_document(
-                documents=testdata.documents,
-                chunk_word_count=5,
-                server_ctx_size=1034,
-            )
-        assert (
-            "Got a larger chunk overlap (100) than chunk size (24), should be smaller"
-            in str(exc.value)
-        )
-
-    def test_chunk_docs_long_lines(self):
-        chunk_words = 50
-        chunks = utils.chunk_document(
-            documents=testdata.long_line_documents,
-            chunk_word_count=chunk_words,
-            server_ctx_size=4096,
-        )
-        max_tokens = utils.num_tokens_from_words(chunk_words)
-        max_chars = utils.num_chars_from_tokens(max_tokens)
-        max_chars += utils.DEFAULT_CHUNK_OVERLAP  # add in the chunk overlap
-        max_chars += 50  # and a bit extra for some really long words
-        for chunk in chunks:
-            assert len(chunk) <= max_chars
 
     @patch(
         "instructlab.utils.git_clone_checkout",
