2588231ba4670c28c265fede28d43567d49cc235
Dan McPherson <dmcphers@redhat.com>
2024-06-26 10:33:44 -0400

    Getting mmlu_branch working
    Signed-off-by: Dan McPherson <dmcphers@redhat.com>

--- src/instructlab/model/evaluate.py
+++ src/instructlab/model/evaluate.py
@@ -72,8 +72,6 @@ def get_evaluator(
         required_args = [model_name, few_shots, batch_size]
         if benchmark == "mmlu_branch":
             required_args.append(sdg_path)
-            required_args.append(taxonomy_path)
-            required_args.append(branch)
         if any(required_args) is None:
             click.secho(
                 f"Benchmark {benchmark} requires the following args to be set: {required_args}",
@@ -86,77 +84,80 @@ def get_evaluator(
             # TODO Make this more robust or wait till we are serving the model from instructlab
             model_dir = os.path.join(os.getcwd(), "models")
             model_path = os.path.join(model_dir, model_name)
-            # tasks = ["mmlu_abstract_algebra","mmlu_anatomy","mmlu_astronomy"]
-            tasks = [
-                "mmlu_abstract_algebra",
-                "mmlu_anatomy",
-                "mmlu_astronomy",
-                "mmlu_business_ethics",
-                "mmlu_clinical_knowledge",
-                "mmlu_college_biology",
-                "mmlu_college_chemistry",
-                "mmlu_college_computer_science",
-                "mmlu_college_mathematics",
-                "mmlu_college_medicine",
-                "mmlu_college_physics",
-                "mmlu_computer_security",
-                "mmlu_conceptual_physics",
-                "mmlu_econometrics",
-                "mmlu_electrical_engineering",
-                "mmlu_elementary_mathematics",
-                "mmlu_formal_logic",
-                "mmlu_global_facts",
-                "mmlu_high_school_biology",
-                "mmlu_high_school_chemistry",
-                "mmlu_high_school_computer_science",
-                "mmlu_high_school_european_history",
-                "mmlu_high_school_geography",
-                "mmlu_high_school_government_and_politics",
-                "mmlu_high_school_macroeconomics",
-                "mmlu_high_school_mathematics",
-                "mmlu_high_school_microeconomics",
-                "mmlu_high_school_physics",
-                "mmlu_high_school_psychology",
-                "mmlu_high_school_statistics",
-                "mmlu_high_school_us_history",
-                "mmlu_high_school_world_history",
-                "mmlu_human_aging",
-                "mmlu_human_sexuality",
-                "mmlu_humanities",
-                "mmlu_international_law",
-                "mmlu_jurisprudence",
-                "mmlu_logical_fallacies",
-                "mmlu_machine_learning",
-                "mmlu_management",
-                "mmlu_marketing",
-                "mmlu_medical_genetics",
-                "mmlu_miscellaneous",
-                "mmlu_moral_disputes",
-                "mmlu_moral_scenarios",
-                "mmlu_nutrition",
-                "mmlu_other",
-                "mmlu_philosophy",
-                "mmlu_prehistory",
-                "mmlu_professional_accounting",
-                "mmlu_professional_law",
-                "mmlu_professional_medicine",
-                "mmlu_professional_psychology",
-                "mmlu_public_relations",
-                "mmlu_security_studies",
-                "mmlu_social_sciences",
-                "mmlu_sociology",
-                "mmlu_stem",
-                "mmlu_us_foreign_policy",
-                "mmlu_virology",
-                "mmlu_world_religions",
-            ]
             if benchmark == "mmlu":
+                min_tasks = os.environ.get("INSTRUCTLAB_EVAL_MMLU_MIN_TASKS")
+                if min_tasks is not None:
+                    tasks = ["mmlu_abstract_algebra","mmlu_anatomy","mmlu_astronomy"]
+                else:
+                    tasks = [
+                    "mmlu_abstract_algebra",
+                    "mmlu_anatomy",
+                    "mmlu_astronomy",
+                    "mmlu_business_ethics",
+                    "mmlu_clinical_knowledge",
+                    "mmlu_college_biology",
+                    "mmlu_college_chemistry",
+                    "mmlu_college_computer_science",
+                    "mmlu_college_mathematics",
+                    "mmlu_college_medicine",
+                    "mmlu_college_physics",
+                    "mmlu_computer_security",
+                    "mmlu_conceptual_physics",
+                    "mmlu_econometrics",
+                    "mmlu_electrical_engineering",
+                    "mmlu_elementary_mathematics",
+                    "mmlu_formal_logic",
+                    "mmlu_global_facts",
+                    "mmlu_high_school_biology",
+                    "mmlu_high_school_chemistry",
+                    "mmlu_high_school_computer_science",
+                    "mmlu_high_school_european_history",
+                    "mmlu_high_school_geography",
+                    "mmlu_high_school_government_and_politics",
+                    "mmlu_high_school_macroeconomics",
+                    "mmlu_high_school_mathematics",
+                    "mmlu_high_school_microeconomics",
+                    "mmlu_high_school_physics",
+                    "mmlu_high_school_psychology",
+                    "mmlu_high_school_statistics",
+                    "mmlu_high_school_us_history",
+                    "mmlu_high_school_world_history",
+                    "mmlu_human_aging",
+                    "mmlu_human_sexuality",
+                    "mmlu_humanities",
+                    "mmlu_international_law",
+                    "mmlu_jurisprudence",
+                    "mmlu_logical_fallacies",
+                    "mmlu_machine_learning",
+                    "mmlu_management",
+                    "mmlu_marketing",
+                    "mmlu_medical_genetics",
+                    "mmlu_miscellaneous",
+                    "mmlu_moral_disputes",
+                    "mmlu_moral_scenarios",
+                    "mmlu_nutrition",
+                    "mmlu_other",
+                    "mmlu_philosophy",
+                    "mmlu_prehistory",
+                    "mmlu_professional_accounting",
+                    "mmlu_professional_law",
+                    "mmlu_professional_medicine",
+                    "mmlu_professional_psychology",
+                    "mmlu_public_relations",
+                    "mmlu_security_studies",
+                    "mmlu_social_sciences",
+                    "mmlu_sociology",
+                    "mmlu_stem",
+                    "mmlu_us_foreign_policy",
+                    "mmlu_virology",
+                    "mmlu_world_religions",
+                ]
                 return evaluator_class(
                     model_path, tasks, "float16", few_shots, batch_size
                 )
             else:
                 return evaluator_class(
-                    model_path, sdg_path, tasks, "float16", few_shots, batch_size
+                    model_path, sdg_path, ["mmlu_pr"], "float16", few_shots, batch_size
                 )
 
 
@@ -275,6 +276,12 @@ def evaluate(
         print(f"qa_pairs length: {len(qa_pairs)}")
 
     elif benchmark == "mmlu":
+        overall_score, individual_scores = evaluator.run()
+        print(f"Overall Score: {overall_score}")
+        print("Individual Scores:")
+        print(individual_scores)
+    
+    elif benchmark == "mmlu_branch":
         overall_score, individual_scores = evaluator.run()
         print(f"Overall Score: {overall_score}")
         print("Individual Scores:")
