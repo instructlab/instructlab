7c992eb6f9cc656b783b052d9d7e91b0c13c5248
Kai Xu <xuk@ibm.com>
2024-07-11 09:10:33 -0400

    format: fix linter
    Signed-off-by: Kai Xu <xuk@ibm.com>

--- src/instructlab/data/generate.py
+++ src/instructlab/data/generate.py
@@ -130,7 +130,11 @@ logger = logging.getLogger(__name__)
 @click.option(
     "--pipeline",
     type=click.STRING,
-    callback=lambda ctx, param, value: value if value in ["simple", "full"] or os.path.isfile(value) else ctx.fail(f'{value} is not a valid pipeline option ("simple" or "full") or file path.'),
+    callback=lambda ctx, param, value: value
+    if value in ["simple", "full"] or os.path.isfile(value)
+    else ctx.fail(
+        f'{value} is not a valid pipeline option ("simple" or "full") or file path.'
+    ),
     default="simple",
     # Hidden until instructlab-sdg releases a version with multiple pipelines
     # For now only "simple" is supported in the latest release.
