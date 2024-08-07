7df607f89b3df4a100ae85417a1fcdd10117dfdc
James Kunstle <jkunstle@redhat.com>
2024-08-07 13:49:55 -0400

    adds a switch so we can save checkpoints on epoch boundary
    currently we save checkpoints via save_samples. This adds the option to
    save at the end of epochs as well.
    
    Signed-off-by: James Kunstle <jkunstle@redhat.com>

--- src/instructlab/configuration.py
+++ src/instructlab/configuration.py
@@ -496,6 +496,7 @@ class _train(BaseModel):
     num_epochs: int
     effective_batch_size: int
     save_samples: int
+    checkpoint_at_epoch: bool = False
 
     deepspeed_cpu_offload_optimizer: bool
 
