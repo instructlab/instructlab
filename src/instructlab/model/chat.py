2c84faafd50a3f2dabfd6f2844a340169da6e966
Charlie Doern <cdoern@redhat.com>
2024-08-20 11:47:54 -0400

    `--model` is ignored in ilab chat when starting server
    select_backend needs `model_path=model` in order to honor `--model` in chat. Otherwise it always uses granite from the serve config.
    
    Signed-off-by: Charlie Doern <cdoern@redhat.com>

--- src/instructlab/model/chat.py
+++ src/instructlab/model/chat.py
@@ -215,7 +215,9 @@ def chat(
         from instructlab.model.backends import backends
 
         ctx.obj.config.serve.llama_cpp.llm_family = model_family
-        backend_instance = backends.select_backend(ctx.obj.config.serve)
+        backend_instance = backends.select_backend(
+            ctx.obj.config.serve, model_path=model
+        )
 
         try:
             # Run the llama server
