74d21ba736044323b34418c07391d3151b38afe0
Costa Shulyupin <costa.shul@redhat.com>
2024-08-28 19:12:37 +0300

    refactor: remove unused argument prompt_file_path
    instructlab.sdg.generate_data doesn't use argument
    `prompt_file_path` anymore.
    
    TODO: remove the argument from generate_data() completely.
    
    Signed-off-by: Costa Shulyupin <costa.shul@redhat.com>

--- tests/test_config.py
+++ tests/test_config.py
@@ -67,7 +67,6 @@ class TestConfig:
         assert cfg.generate.sdg_scale_factor == 30
         assert cfg.generate.chunk_word_count == 1000
         assert cfg.generate.output_dir == f"{data_dir}/datasets"
-        assert cfg.generate.prompt_file == f"{data_dir}/{internal_dirname}/prompt.txt"
         assert (
             cfg.generate.seed_file == f"{data_dir}/{internal_dirname}/seed_tasks.json"
         )
