d778b3d30eed1597d273176ab38dcd3d8e841985
reid_liu <guliu@redhat.com>
2024-09-13 10:02:28 -0700

    use pretty color for config show
    Signed-off-by: reid_liu <guliu@redhat.com>

--- src/instructlab/config/show.py
+++ src/instructlab/config/show.py
@@ -1,10 +1,12 @@
 # SPDX-License-Identifier: Apache-2.0
+
 # Standard
-import sys
 
 # Third Party
-from ruamel.yaml import YAMLError, ruamel
+from ruamel.yaml import YAMLError, ruamel, compat
 import click
+from rich.console import Console
+from rich.syntax import Syntax
 
 # First Party
 from instructlab import clickext, configuration
@@ -19,10 +21,14 @@ yaml.indent(mapping=2, sequence=4, offset=2)
 @clickext.display_params
 def show(ctx: click.Context) -> None:
     """Displays the current config as YAML"""
-    # TODO: make this use pretty colors like jq/yq
+    # use pretty colors like jq/yq
+    console = Console()
     try:
         commented_map = configuration.config_to_commented_map(ctx.obj.config)
+        stream = compat.StringIO()
+        yaml.dump(commented_map, stream)
+        syntax = Syntax(stream.getvalue(), "yaml", theme="monokai", line_numbers=True)
+        console.print(syntax)
     except YAMLError as e:
         click.secho(f"Error loading config as YAML: {e}", fg="red")
         ctx.exit(2)
-    yaml.dump(commented_map, sys.stdout)
