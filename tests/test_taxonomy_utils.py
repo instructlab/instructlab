751de99e13363d15f41023c84c61fb2a3830b570
Daniele Martinoli <dmartino@redhat.com>
2025-01-15 10:51:41 +0100

    added sleep time to taxonomy test
    Signed-off-by: Daniele Martinoli <dmartino@redhat.com>

--- tests/test_taxonomy_utils.py
+++ tests/test_taxonomy_utils.py
@@ -1,6 +1,7 @@
 # Standard
 import os
 import tempfile
+import time
 
 # First Party
 from instructlab.rag.taxonomy_utils import lookup_processed_documents_folder
@@ -10,6 +11,7 @@ def test_lookup_processed_documents_folder_finds_latest_folder():
     with tempfile.TemporaryDirectory() as tmpdir:
         # Create folder structure to navigate into
         os.makedirs(os.path.join(tmpdir, "first/docling-artifacts"))
+        time.sleep(1)
         os.makedirs(os.path.join(tmpdir, "second/docling-artifacts"))
 
         latest_doc_folder = lookup_processed_documents_folder(tmpdir)
@@ -22,6 +24,7 @@ def test_lookup_processed_documents_folder_returns_None_when_no_documents():
     with tempfile.TemporaryDirectory() as tmpdir:
         # Create folder structure to navigate into
         os.makedirs(os.path.join(tmpdir, "first/docling-artifacts"))
+        time.sleep(1)
         os.makedirs(os.path.join(tmpdir, "second"))
 
         latest_doc_folder = lookup_processed_documents_folder(tmpdir)
