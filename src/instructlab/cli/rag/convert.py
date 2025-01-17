be9101b61be2a71962f4d682acfbfd003f7715e1
Bill Murdock <bmurdock@redhat.com>
2025-01-17 13:07:21 -0500

    feat(rag): Add lab rag convert CLI command
    Signed-off-by: Bill Murdock <bmurdock@redhat.com>

---
+++ src/instructlab/cli/rag/convert.py
@@ -0,0 +1,80 @@
+# SPDX-License-Identifier: Apache-2.0
+
+# Code to instantiate the ilab rag convert command.
+# Calls out to code in ../../rag/convert.py to
+# provide the implementation.
+
+# Standard
+import logging
+import os
+
+# Third Party
+import click
+
+# First Party
+from instructlab import clickext
+from instructlab.rag.convert import (
+    convert_documents_from_folder,
+    convert_documents_from_taxonomy,
+)
+
+logger = logging.getLogger(__name__)
+
+
+@click.command()
+@click.option(
+    "--input-dir",
+    required=False,
+    type=click.Path(exists=True, file_okay=False, dir_okay=True, readable=True),
+    default=None,
+    help="A folder with user documents to process. If it set, the taxonomy is ignored.  If it's missing, the knowledge taxonomy files will be processed instead.",
+)
+@click.option(
+    "--taxonomy-path",
+    required=True,
+    type=click.Path(file_okay=False, readable=True),
+    config_class="generate",
+    cls=clickext.ConfigOption,
+)
+@click.option(
+    "--taxonomy-base",
+    required=True,
+    config_class="generate",
+    cls=clickext.ConfigOption,
+)
+@click.option(
+    "--output-dir",
+    type=click.Path(file_okay=False, writable=True),
+    required=True,
+    config_class="rag",
+    config_sections="convert",
+    cls=clickext.ConfigOption,
+)
+@click.pass_context
+@clickext.display_params
+def convert(
+    ctx,
+    taxonomy_path,
+    taxonomy_base,
+    input_dir,
+    output_dir,
+):
+    """Pipeline to convert documents from their original format (e.g., PDF) into Docling JSON format for use by ilab rag ingest"""
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    if input_dir is None:
+        logger.info(
+            f"Pre-processing latest taxonomy changes at {taxonomy_path}@{taxonomy_base}"
+        )
+        convert_documents_from_taxonomy(
+            taxonomy_path=taxonomy_path,
+            taxonomy_base=taxonomy_base,
+            output_dir=output_dir,
+        )
+    else:
+        logger.info(f"Pre-processing documents from {input_dir} to {output_dir}")
+        convert_documents_from_folder(
+            input_dir=input_dir,
+            output_dir=output_dir,
+        )
---
+++ tests/test_lab_rag_convert.py
@@ -0,0 +1,164 @@
+# SPDX-License-Identifier: Apache-2.0
+
+# Standard
+from pathlib import Path
+from typing import Dict, Iterable, Iterator, List, Optional, Union
+from unittest.mock import patch
+
+# Third Party
+from click.testing import CliRunner
+from docling.backend.docling_parse_v2_backend import (  # type: ignore  # noqa: F401
+    DoclingParseV2DocumentBackend,
+)
+from docling.datamodel.base_models import DocumentStream  # type: ignore  # noqa: F401
+from docling.datamodel.base_models import (  # type: ignore  # noqa: F401
+    ConversionStatus,
+    InputFormat,
+)
+from docling.datamodel.document import ConversionResult  # type: ignore  # noqa: F401
+from docling.datamodel.document import InputDocument  # type: ignore  # noqa: F401
+from docling.document_converter import FormatOption  # type: ignore  # noqa: F401
+
+# First Party
+from instructlab import lab
+from instructlab.rag.convert import _load_converter_and_format_options
+
+
+class MockDocumentConverter:
+    def __init__(
+        self,
+        allowed_formats: Optional[list[InputFormat]] = None,  # pylint: disable=unused-argument; noqa: ARG002
+        format_options: Optional[Dict[InputFormat, FormatOption]] = None,  # pylint: disable=unused-argument; noqa: ARG002
+    ):
+        pass
+
+    def convert_all(
+        self,
+        source: Iterable[Union[Path, str, DocumentStream]],  # pylint: disable=unused-argument; noqa: ARG002
+        raises_on_error: bool = True,  # pylint: disable=unused-argument; noqa: ARG002
+    ) -> Iterator[ConversionResult]:
+        # Third Party
+        for doc in source:
+            print(type(doc))
+            in_doc = InputDocument(
+                path_or_stream=doc,
+                format=InputFormat.PDF,
+                backend=DoclingParseV2DocumentBackend,
+            )
+            conv_res = ConversionResult(input=in_doc, status=ConversionStatus.SUCCESS)
+            yield conv_res
+
+
+def run_rag_convert_test(
+    params: List[str],
+    expected_strings: List[str],  # pylint: disable=unused-argument; noqa: ARG002
+    expected_output_file: Union[Path, None],  # pylint: disable=unused-argument; noqa: ARG002
+    should_succeed: bool,
+):
+    """
+    Core logic for testing conversion using the CLI runner and checking for expected output string and expected output file.
+    params = A list of parameters that would follow 'ilab rag config' on the command line.
+    expected_strings = A list of strings that are expected to appear in the output when running with these parameters.
+    expected_output_file = A fully qualified path to a file we would expect to be generated from running with these parameters.
+    should_succeed = True iff the we would expect the command to succeed (complete with a 0 exit code).
+    """
+    _load_converter_and_format_options()
+    runner = CliRunner()
+    with patch("instructlab.rag.convert.DocumentConverter", MockDocumentConverter):
+        result = runner.invoke(
+            lab.ilab, ["--config=DEFAULT", "rag", "convert"] + params
+        )
+        if should_succeed:
+            assert (
+                result.exit_code == 0
+            ), f"Unexpected failure for parameters {params}: {result.output}"
+        for expected_string in expected_strings:
+            assert expected_string in result.output
+        if expected_output_file is not None:
+            assert expected_output_file.exists()
+        else:
+            assert (
+                result.exit_code != 0
+            ), f"Unexpected success for parameters {params}: {result.output}"
+
+
+def test_convert_pdf_from_directory(tmp_path: Path):
+    """
+    Tests converting from the sample PDF in tests/testdata/documents/pdf.
+    Verifies that it says that it is processing and finished that sample PDF.
+    Also verifies that the expected JSON file exists in the temp directory at the end.
+    """
+    test_input_dir = "tests/testdata/documents/pdf"
+    test_output_dir = tmp_path / "convert-outputs"
+    params = ["--input-dir", str(test_input_dir), "--output-dir", str(test_output_dir)]
+    expected_strings = ["How to use YAML with InstructLab Page 1.pdf"]
+    expected_output_file = (
+        test_output_dir / "How to use YAML with InstructLab Page 1.json"
+    )
+    run_rag_convert_test(params, expected_strings, expected_output_file, True)
+
+
+def test_convert_md_from_directory(tmp_path: Path):
+    """
+    Tests converting from the sample Markdown file in tests/testdata/documents/md.
+    Verifies that the expected JSON file exists in the temp directory at the end.
+    """
+    test_input_dir = "tests/testdata/documents/md"
+    test_output_dir = tmp_path / "convert-outputs"
+    params = ["--input-dir", str(test_input_dir), "--output-dir", str(test_output_dir)]
+    expected_strings = ["Transforming source files ['hello.md']"]
+    expected_output_file = test_output_dir / "hello.json"
+    run_rag_convert_test(params, expected_strings, expected_output_file, True)
+
+
+# Note: This test uses a local taxonomy that references a file in a remote git repository.  For that reason, this
+# test won't pass when run on a machine with no connection to the internet.  It wil also fail if the repository
+# is not working or if it ever gets deleted.  That's not ideal, but we do need to test these capabilities.
+# TODO: Consider re-working this with a mock for the github server.
+def test_convert_md_from_taxonomy(tmp_path: Path):
+    """
+    Tests converting from the sample Markdown file in a github repo referenced in tests/testdata/sample_taxonomy.
+    The specific file referenced is phoenix.md in https://github.com/RedHatOfficial/rhelai-taxonomy-data,
+    because that is the one in
+    https://github.com/instructlab/sdg/blob/main/tests/testdata/test_valid_knowledge_skill.yaml.
+    Verifies that the expected JSON file exists in the temp directory at the end.
+    """
+    test_input_taxonomy = "tests/testdata/sample_taxonomy"
+    test_output_dir = tmp_path / "convert-outputs"
+    params = [
+        "--taxonomy-path",
+        str(test_input_taxonomy),
+        "--taxonomy-base",
+        "empty",
+        "--output-dir",
+        str(test_output_dir),
+    ]
+    expected_strings = ["Transforming source files ['phoenix.md']"]
+    expected_output_file = test_output_dir / "phoenix.json"
+    run_rag_convert_test(params, expected_strings, expected_output_file, True)
+
+
+# Note that there is no test for converting pdf from taxonomy.  The tests above verify that you can convert
+# both PDF and md and that you can convert from both a directory and a taxonomy.  Testing a PDF from a
+# taxonomy too seems redundant, and these tests are already taking a lot of time so I don't want to add
+# another expensive one for that purpose.
+
+
+def test_convert_from_missing_directory_fails(tmp_path: Path):
+    """
+    Verifies that converting from a non-existent directory fails.
+    """
+    test_input_dir = "tests/testdata/documents/no-such-directory"
+    test_output_dir = tmp_path / "convert-outputs"
+    params = ["--input-dir", str(test_input_dir), "--output-dir", str(test_output_dir)]
+    run_rag_convert_test(params, [], None, False)
+
+
+def test_convert_from_non_directory_fails(tmp_path: Path):
+    """
+    Verifies that converting fails when the input directory is a file and not a directory.
+    """
+    test_input_dir = "tests/testdata/documents/md/hello.md"
+    test_output_dir = tmp_path / "convert-outputs"
+    params = ["--input-dir", str(test_input_dir), "--output-dir", str(test_output_dir)]
+    run_rag_convert_test(params, [], None, False)
