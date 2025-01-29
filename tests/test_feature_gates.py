de6d3456065b35c63dd5039b75f07f1fa198fdff
Anastas Stoyanovsky <astoyano@redhat.com>
2025-01-29 14:46:10 -0500

    Fix copy pasted mistake in feature gating unit test
    Signed-off-by: Anastas Stoyanovsky <astoyano@redhat.com>

--- tests/test_feature_gates.py
+++ tests/test_feature_gates.py
@@ -29,5 +29,9 @@ def test_experimental_features_off_when_environment_variable_not_set():
     assert not FeatureGating.feature_available(GatedFeatures.RAG)
 
 
+@mock.patch.dict(
+    os.environ,
+    {FeatureGating.env_var_name: FeatureScopes.Default.value},
+)
 def test_experimental_features_off_in_default_feature_scope():
     assert not FeatureGating.feature_available(GatedFeatures.RAG)
