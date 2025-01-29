539c24f8502195a81fc68b13e8e4f0ce5854a205
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2025-01-29 09:43:17 -0500

    ui: Humanize help message describing available command groups.
    - Removed duplicate 'Command group for'.
    - Removed most references to 'InstructLab' (the context is already
      clear). (Left for `config` section where it's important to note that
      it's the tool configuration and not e.g. system configuration.)
    - Used more diverse set of verbs (not just 'interact').
    
    Signed-off-by: Ihar Hrachyshka <ihar.hrachyshka@gmail.com>

--- src/instructlab/taxonomy/taxonomy.py
+++ src/instructlab/taxonomy/taxonomy.py
@@ -16,7 +16,7 @@ from instructlab.configuration import storage_dirs_exist
 )
 @click.pass_context
 def taxonomy(ctx):
-    """Command group for interacting with the taxonomy of InstructLab.
+    """Manage taxonomy datasets.
 
     If this is your first time running ilab, it's best to start with `ilab config init` to create the environment.
     """
