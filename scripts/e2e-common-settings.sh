eca869af76ac748db557f3a2d421d512d9a2e226
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2025-06-09 17:44:47 +0000

    ci: don't complain about lack of p2p between GPUs
    Signed-off-by: Ihar Hrachyshka <ihar.hrachyshka@gmail.com>

--- scripts/e2e-common-settings.sh
+++ scripts/e2e-common-settings.sh
@@ -8,6 +8,9 @@ export TORCH_DISTRIBUTED_DEBUG=INFO
 export NCCL_DEBUG=INFO
 export NCCL_DEBUG_SUBSYS=INIT,BOOTSTRAP,ENV
 
+# We don't run on nvlink-enabled ec2 runners
+export NCCL_IGNORE_DISABLED_P2P=1
+
 # Dump debug info, including full stack traces, on watchdog timeout and collective unsync
 export TORCH_NCCL_DUMP_ON_TIMEOUT=1
 export TORCH_SHOW_CPP_STACKTRACES=1
